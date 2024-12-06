7429826c955e6458df2d13c512270cc9da322da9
Rmrti <mutuma34@gmail.com>
2024-12-06 17:36:28 +0300

    Add functions for case-insensitive name matching, prefix matching, and object-based name matching

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solution here
+function findMatching(source, sought) {
+    return source.filter((possibleMatch) => {
+      return possibleMatch.toLowerCase() === sought.toLowerCase();
+    });
+  }
+  
+  function fuzzyMatch(source, testString) {
+    return source.filter((possibleMatch) => {
+      return possibleMatch.toLowerCase().startsWith(testString.toLowerCase());
+    });
+  }
+  
+  function matchName(source, soughtName) {
+    return source.filter((record) => {
+      return record.name === soughtName;
+    });
+  }
+  
